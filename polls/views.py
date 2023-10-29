750c1a79319d24ec62aa30ea1a6e184db542749a
simondelali <atiegardelali@gmail.com>
2023-10-29 12:30:10 +0000

    add more views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404 , HttpResponse
 from .models import Questions
 
 # Create your views here.
@@ -12,7 +12,10 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Questions, pk=question_id)
+    context = {"question":question}
+    
+    return render(request, "polls/detail.html", context)
 
 
 def results(request, question_id):
